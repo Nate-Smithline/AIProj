0fe2db2f74c241d1f9fc25d6010d84c18182f8d5
Nate-Smithline <nathan_m_smith@outlook.com>
2024-03-25 18:48:41 -0400

    added details on design choices

--- Astar.py
+++ Astar.py
@@ -9,9 +9,14 @@ class ATree:
 class Astar:
     def __init__(self, filename):
         self.gameBoard = [[0] * 50 for _ in range(30)]
+        # will be of type (i, j)
         self.visitedNodes = []
+        # will be of type Node() => f(n) value
         self.viableOptions = {}
         self.numVisited = 0
+        # will be of type (i, j)
         self.currentPosition = []
+        # will be of type (i, j)
         self.goalPosotion = []
+        # not sure if necessary, basic linked list/tree
         self.tree = ATree()
